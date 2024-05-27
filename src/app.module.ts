5b13f1494f660640a229715ddccb09979d7bb697
RMKaelBR <rmbresurreccion@gmail.com>
2024-05-27 11:44:43 +0800

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule],
+  controllers: [AppController, UsersController],
+  providers: [AppService],
+})
+export class AppModule {}
